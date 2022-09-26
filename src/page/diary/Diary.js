86585d9b919d242960274f2c0fce6a01ffd7de10
김민지 <wehouse201@naver.com>
2022-09-26 17:40:52 +0900

    re

--- src/page/diary/Diary.js
+++ src/page/diary/Diary.js
@@ -4,61 +4,11 @@ import * as D from '../../style/diary.js'
 
 function Diary(){
 
-  const face=[
-    {
-      id:0,
-      img:'/face/people.svg',
-      date:'2022/1/2',
-    },
-    {
-      id:2,
-      img:'/face/people.svg',
-      date:'2022/1/4',
-    },
-  ]
-
-
   return(
     <>
-
     <Header/>
-    <D.diary>
-
-      <D.dbox>
-      <D.diarybox>
-          <D.top><D.face src='/face/people.svg'/></D.top>
-          
-          <D.bottom>
-            <D.title>제목</D.title>
-            <D.date>날짜</D.date>
-            <D.go>확인하기</D.go>
-          </D.bottom>
-      </D.diarybox>
-
-      <D.diarybox>
-          <D.top><D.face src='/face/people.svg'/></D.top>
-          <D.bottom>
-            <D.title>제목</D.title>
-            <D.date>날짜</D.date>
-            <D.go>확인하기</D.go>
-          </D.bottom>
-      </D.diarybox>
-
-      <D.diarybox>
-          <D.top><D.face src='/face/people.svg'/></D.top>
-          
-          <D.bottom>
-            <D.title>제목</D.title>
-            <D.date>날짜</D.date>
-            <D.go>확인하기</D.go>
-          </D.bottom>
-      </D.diarybox>
-      </D.dbox>
-
-    </D.diary>
     
     <Button/>
-
     </>
   )
 }
