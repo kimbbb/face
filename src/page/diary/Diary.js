d5b2c32cbc0b0a4f2d6a621ac78db868695bdcbf
김민지 <wehouse201@naver.com>
2022-09-26 20:13:59 +0900

    다이어리

--- src/page/diary/Diary.js
+++ src/page/diary/Diary.js
@@ -1,13 +1,76 @@
 import Header from '../../components/header/Header2.js';
-import Button from '../../components/Button.js'
-import * as D from '../../style/diary.js'
+import Button from '../../components/Button.js';
+import * as D from '../../style/diary.js';
+
 
 function Diary(){
 
   return(
     <>
     <Header/>
-    
+    <D.box>
+      
+    <D.card>
+        <D.top>
+          <D.image src='/face/people.svg'/>
+        </D.top>
+
+        <D.bottom>
+          <D.title>제목</D.title>
+          <D.p>날짜</D.p>
+          <D.check>확인</D.check>
+        </D.bottom>
+      </D.card>
+
+      <D.card>
+        <D.top>
+          <D.image src='/face/people.svg'/>
+        </D.top>
+
+        <D.bottom>
+          <D.title>제목</D.title>
+          <D.p>날짜</D.p>
+          <D.check>확인</D.check>
+        </D.bottom>
+      </D.card>
+
+      <D.card>
+        <D.top>
+          <D.image src='/face/people.svg'/>
+        </D.top>
+
+        <D.bottom>
+          <D.title>제목</D.title>
+          <D.p>날짜</D.p>
+          <D.check>확인</D.check>
+        </D.bottom>
+      </D.card>
+
+      <D.card>
+        <D.top>
+          <D.image src='/face/people.svg'/>
+        </D.top>
+
+        <D.bottom>
+          <D.title>제목</D.title>
+          <D.p>날짜</D.p>
+          <D.check>확인</D.check>
+        </D.bottom>
+      </D.card>
+
+      <D.card>
+        <D.top>
+          <D.image src='/face/people.svg'/>
+        </D.top>
+
+        <D.bottom>
+          <D.title>제목</D.title>
+          <D.p>날짜</D.p>
+          <D.check>확인</D.check>
+        </D.bottom>
+      </D.card>
+
+    </D.box>
     <Button/>
     </>
   )
--- src/style/diary.js
+++ src/style/diary.js
@@ -1,1 +1,66 @@
 import styled from "styled-components";
+
+export const box=styled.div`
+  width:80%;
+  position: relative;
+  top:140px;
+  left: 13%;
+`
+
+export const card = styled.div`
+  position:relative;
+  float:left;
+  width:320px;
+  height:400px;
+  background-color:#fff;
+  border-radius:20px;
+  margin:20px;
+  &:hover{
+    outline:4px solid #BE97FF;
+  }
+`
+
+export const top=styled.div`
+  position:absolute;
+  width:100%;
+  height:50%;
+  background-color:#E9ECEF;
+  border-radius:20px 20px 0px 0px;
+  display:flex;
+  align-items:center;
+`
+
+export const image=styled.img`
+  width:100%;
+  height:70%;
+`
+
+export const bottom=styled.div`
+  position:absolute;
+  width:100%;
+  height:50%;
+  bottom:0;
+  padding:20px;
+`
+
+export const title=styled.h3`
+  font-weight:700;
+`
+
+export const p = styled.p`
+  font-size:18px;
+`
+
+export const check= styled.button`
+  width:140px;
+  height:50px;
+  background-color:#904DFF;
+  color:#fff;
+  border-radius:20px;
+  border:none;
+  &:hover{
+    &:focus{
+      outline:4px solid #BE97FF;
+    }
+  } 
+`
