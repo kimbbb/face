56ca55c6e574bc6edc2493d82f1808d7b7426fab
김민지 <wehouse201@naver.com>
2022-10-19 16:52:15 +0900

    about

--- src/page/about/About.js
+++ src/page/about/About.js
@@ -1,12 +1,53 @@
 import Header from '../../components/header/Header3';
 import aboutimg from '../../image/about.svg'
 import Button from '../../components/Button.js';
+import styled from 'styled-components';
+
+const Aboutbg = styled.img`
+  width:100%;
+  animation: fadein 3s;
+  -moz-animation: fadein 3s;
+  -webkit-animation: fadein 3s; 
+  -o-animation: fadein 3s; 
+  @keyframes fadein {
+    from {
+        opacity: 0;
+    }
+    to {
+        opacity: 1;
+    }
+  }
+  @-moz-keyframes fadein { /* Firefox */
+      from {
+          opacity: 0;
+      }
+      to {
+          opacity: 1;
+      }
+  }
+  @-webkit-keyframes fadein { /* Safari and Chrome */
+      from {
+          opacity: 0;
+      }
+      to {
+          opacity: 1;
+      }
+  }
+  @-o-keyframes fadein { /* Opera */
+      from {
+          opacity: 0;
+      }
+      to {
+          opacity: 1;
+      }
+  }
+`
 
 function About() {
   return(
     <>
     <Header/>
-    <img style={{width: '100%'}} src={aboutimg}/>
+      <Aboutbg src={aboutimg}/>
     <Button/>
     </>
   )
